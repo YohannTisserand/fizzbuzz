fe3ba23acff869ff323931e7258296c61dfe81ad
YohannTisserand <tisserand.yohann@gmail.com>
2021-03-22 17:03:25 +0000

    fizzbuzz done

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,6 @@
 def fizzbuzz(int)
-    if int % 15 == 0
-      return 'fizzbuzz'
-    elsif int % 3 == 0
-         'fizz'
-    elsif int % 5 == 0
-         'buzz'
-    end
+  return 'fizzbuzz' if int % 15 == 0
+  return 'fizz' if int % 3 == 0
+  return 'buzz' if int % 5 == 0
+  return int
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -12,4 +12,8 @@ describe 'fizzbuzz' do
   it 'should return fizzbuzz' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
+
+  it 'should return a number' do
+    expect(fizzbuzz(7)).to eq(7)
+  end
 end
